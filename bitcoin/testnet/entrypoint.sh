1dbb34c2fa1a39dde168a8f022441cbe70c3eac5
syrohei <syrohei@gmail.com>
2020-09-13 12:01:20 +0900

    add -workers=1

--- bitcoin/testnet/entrypoint.sh
+++ bitcoin/testnet/entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd /opt/coins/blockbook/bitcoin_testnet/
 
-./bin/blockbook -sync -blockchaincfg=/app/blockchaincfg.json -internal=:9030 -public=:9130 -logtostderr
+./bin/blockbook -sync -blockchaincfg=/app/blockchaincfg.json -internal=:9030 -public=:9130 -workers=1 -logtostderr
