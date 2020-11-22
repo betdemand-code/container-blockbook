6e604fecbe64fb36214eb570acd94290032e0475
syrohei <syrohei@gmail.com>
2020-11-22 20:18:12 +0900

    feat: Add -resyncmempoolperiod=5000 to both network

--- coins/ethereum/testnet/entrypoint.sh
+++ coins/ethereum/testnet/entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd /opt/coins/blockbook/ethereum/
 
-./bin/blockbook -sync -blockchaincfg=/app/blockchaincfg.json -internal=:9030 -resyncindexperiod=5000 -public=:9130 -workers=1 -logtostderr
+./bin/blockbook -sync -blockchaincfg=/app/blockchaincfg.json -internal=:9030 -resyncindexperiod=5000 -resyncmempoolperiod=5000 -public=:9130 -workers=1 -logtostderr
