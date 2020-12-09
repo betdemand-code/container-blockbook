e322774f3366fb7e0d8ac02b321169ea3489f47b
syrohei <syrohei@gmail.com>
2020-12-09 16:36:57 +0900

    fix: changed all configs

--- coins/bitcoin/testnet/entrypoint.sh
+++ coins/bitcoin/testnet/entrypoint.sh
@@ -2,4 +2,11 @@
 
 cd /opt/coins/blockbook/bitcoin_testnet/
 
-./bin/blockbook -sync -blockchaincfg=/app/blockchaincfg.json -internal=:9030 -public=:9130 -resyncindexperiod=5000 -resyncmempoolperiod=5000 -workers=1 -logtostderr
+./bin/blockbook -sync \
+    -blockchaincfg=/app/blockchaincfg.json \
+    -internal=:9030 \
+    -public=:9130 \
+    -resyncindexperiod=5000 \
+    -resyncmempoolperiod=5000 \
+    -workers=1 \
+    -logtostderr
