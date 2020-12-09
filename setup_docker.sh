b5aea27bfffff4f78f92107196df176750944097
syrohei <syrohei@gmail.com>
2020-12-09 15:02:07 +0900

    fix: Added script to setup swap mem

--- setup_docker.sh
+++ setup_docker.sh
@@ -8,3 +8,9 @@ if ! type "docker-compose" >/dev/null; then
     sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
+# Make swap file
+fallocate -l 5G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile swap swap defaults 0 0' >>/etc/fstab
