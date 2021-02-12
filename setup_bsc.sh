64ac0500b3bf04b339a643fb0c0e59b82c1fc866
syrohei <syrohei@gmail.com>
2021-02-12 18:26:45 +0900

    fix/bsc: changed dir for deployemnt

--- setup_bsc.sh
+++ setup_bsc.sh
@@ -11,18 +11,18 @@ if [[ "$testnet" == "" ]]; then
 fi
 
 if [[ "$testnet" == "true" ]]; then
-    if [ -e "$dir/bsc_testnet/genesis.json" ]; then
+    if [ -e "$dir/testnet/bsc_testnet/genesis.json" ]; then
         exit 0
     fi
     wget --no-check-certificate $(curl -s https://api.github.com/repos/binance-chain/bsc/releases/latest | grep browser_ | grep testnet | cut -d\" -f4)
     unzip testnet.zip
-    mkdir -p "$dir/bsc_testnet"
-    cp testnet/config.yaml "$dir/bsc_testnet/config.yaml"
-    cp testnet/genesis.json "$dir/bsc_testnet/genesis.json"
+    mkdir -p "$dir/testnet/bsc_testnet"
+    cp testnet/config.yaml "$dir/testnet/bsc_testnet/config.yaml"
+    cp testnet/genesis.json "$dir/testnet/bsc_testnet/genesis.json"
     DIR=$dir docker-compose run bsc --datadir /home/geth init /home/geth/genesis.json 
 fi
 if [[ "$testnet" == "false" ]]; then
-    if [ -e "$dir/bsc_mainnet/genesis.json" ]; then
+    if [ -e "$dir/mainnet/bsc_mainnet/genesis.json" ]; then
         exit 0
     fi
     wget --no-check-certificate $(curl -s https://api.github.com/repos/binance-chain/bsc/releases/latest | grep browser_ | grep mainnet | cut -d\" -f4)
