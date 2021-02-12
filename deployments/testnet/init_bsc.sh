89dd6bb5fc36696d46cee3b10d9c55bbb803bbf9
syrohei <syrohei@gmail.com>
2021-02-12 18:35:53 +0900

    feat/script: added script for bsc initialize

---
+++ deployments/testnet/init_bsc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+DIR=$dir docker-compose run bsc --datadir /home/geth init /home/geth/genesis.json
--- setup_bsc.sh
+++ setup_bsc.sh
@@ -3,8 +3,10 @@
 dir=$DIR
 testnet=$TESTNET
 
-apt-get update
-apt-get install -y wget curl unzip
+if ! type "unzip" >/dev/null; then
+    apt-get update
+    apt-get install -y wget curl
+fi
 
 if [[ "$testnet" == "" ]]; then
     testnet="false"
@@ -14,12 +16,10 @@ if [[ "$testnet" == "true" ]]; then
     if [ -e "$dir/testnet/bsc_testnet/genesis.json" ]; then
         exit 0
     fi
+    mkdir -p "$dir/testnet/bsc_testnet"
+    cd "$dir/testnet/bsc_testnet"
     wget --no-check-certificate $(curl -s https://api.github.com/repos/binance-chain/bsc/releases/latest | grep browser_ | grep testnet | cut -d\" -f4)
     unzip testnet.zip
-    mkdir -p "$dir/testnet/bsc_testnet"
-    cp testnet/config.yaml "$dir/testnet/bsc_testnet/config.yaml"
-    cp testnet/genesis.json "$dir/testnet/bsc_testnet/genesis.json"
-    DIR=$dir docker-compose run bsc --datadir /home/geth init /home/geth/genesis.json 
 fi
 if [[ "$testnet" == "false" ]]; then
     if [ -e "$dir/mainnet/bsc_mainnet/genesis.json" ]; then
