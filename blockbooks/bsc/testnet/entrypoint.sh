2094b7f743d220a15a39a7a32e61fb8ecf4ef423
syrohei <syrohei@gmail.com>
2021-02-12 17:44:16 +0900

    feat/bsc: added bsc docker compose scirpt

---
+++ blockbooks/bsc/testnet/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /opt/coins/blockbook/ethereum_testnet_goerli/
+
+./bin/blockbook -sync \
+    -blockchaincfg=/app/blockchaincfg.json \
+    -internal=:9030 \
+    -public=:9130 \
+    -resyncindexperiod=5000 \
+    -resyncmempoolperiod=5000 \
+    -workers=1 \
+    -logtostderr
