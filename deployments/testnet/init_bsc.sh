9fadaf5e00a8e9c55ff9c18ce7ba3ee7493364ab
syrohei <syrohei@gmail.com>
2021-02-23 20:21:56 +0900

    fix/bsc: setup script

--- deployments/testnet/init_bsc.sh
+++ deployments/testnet/init_bsc.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-DIR=$DIR docker-compose run --rm bsc --datadir /home/geth init /home/geth/genesis.json
+if ! [ -e "$dir/testnet/bsc_testnet/genesis.json" ]; then
+    DIR=$DIR docker-compose run --rm bsc --datadir /home/geth init /home/geth/genesis.json
+fi
