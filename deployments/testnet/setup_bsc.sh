606c1d8c435c4f21619bf827542668e47d28b152
syrohei <syrohei@gmail.com>
2021-02-23 20:46:12 +0900

    fix/script: added unzip install cmd

--- deployments/testnet/setup_bsc.sh
+++ deployments/testnet/setup_bsc.sh
@@ -3,7 +3,7 @@
 dir=$DIR
 if ! type "unzip" >/dev/null; then
     apt-get update
-    apt-get install -y wget curl
+    apt-get install -y wget curl unzip
 fi
 
 if [ -e "$dir/testnet/bsc_testnet/genesis.json" ]; then
@@ -14,3 +14,4 @@ cd "$dir/testnet/bsc_testnet"
 wget --no-check-certificate $(curl -s https://api.github.com/repos/binance-chain/bsc/releases/latest | grep browser_ | grep testnet | cut -d\" -f4)
 unzip testnet.zip
 rm *.zip
+
