d91649cd53158724b0456fbf9cb070cfa6900ad4
syrohei <syrohei@gmail.com>
2021-03-22 14:28:26 +0900

    fix/init_bsc: changed env params on init script

--- deployments/mainnet/init_bsc.sh
+++ deployments/mainnet/init_bsc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if ! [ -e "$dir/mainnet/bsc/genesis.json" ]; then
+if ! [ -e "$DIR/mainnet/bsc/genesis.json" ]; then
     DIR=$DIR docker-compose run --rm bsc --datadir /home/geth init /home/geth/genesis.json
 fi
--- deployments/testnet/init_bsc.sh
+++ deployments/testnet/init_bsc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if ! [ -e "$dir/testnet/bsc_testnet/genesis.json" ]; then
+if ! [ -e "$DIR/testnet/bsc_testnet/genesis.json" ]; then
     DIR=$DIR docker-compose run --rm bsc --datadir /home/geth init /home/geth/genesis.json
 fi
