52ba0d24a5c5f2c29fc9f8af3ca299ed1b990e75
syrohei <syrohei@gmail.com>
2021-03-23 14:34:18 +0900

    fix: changed init dir path

--- deployments/mainnet/init_bsc.sh
+++ deployments/mainnet/init_bsc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if ! [ -e "$DIR/mainnet/bsc/genesis.json" ]; then
-    DIR=$DIR docker-compose run --rm bsc --datadir /home/geth init /home/geth/genesis.json
+    DIR=$DIR docker-compose run --rm bsc --datadir /home/geth/data init /home/geth/genesis.json
 fi
