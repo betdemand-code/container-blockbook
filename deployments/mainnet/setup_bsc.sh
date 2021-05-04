30a24c1a611d617135732cd1d79ac97f6762be50
Yusaku Senga <yusaku@swingby.network>
2021-05-05 00:04:41 +0900

    Update setup_bsc.sh

--- deployments/mainnet/setup_bsc.sh
+++ deployments/mainnet/setup_bsc.sh
@@ -11,8 +11,8 @@ if ! [ -e "$dir/mainnet/bsc/genesis.json" ]; then
 fi
 cd "$dir/mainnet/bsc"
 wget --no-check-certificate $(curl -s https://api.github.com/repos/binance-chain/bsc/releases/latest | grep browser_ | grep mainnet | cut -d\" -f4)
-unzip mainnet.zip
-rm mainnet.zip
-rm mainnet.zip.*
+unzip -o mainnet.zip
+rm -f mainnet.zip
+rm -f mainnet.zip.*
 
 sed -i '/HTTPHost/d' "$dir/mainnet/bsc/config.toml"
