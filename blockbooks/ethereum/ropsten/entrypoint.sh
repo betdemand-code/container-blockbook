cbedd77448d3c5e730bccbee9190623b1180fad0
syrohei <syrohei@gmail.com>
2021-09-20 13:20:14 +0800

    feat: added ropsten

---
+++ blockbooks/ethereum/ropsten/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /opt/coins/blockbook/ethereum_testnet_goerli/
+
+./bin/blockbook -sync \
+    -blockchaincfg=/app/blockchaincfg.json \
+    -internal=:9030 \
+    -public=:9130 \
+    -resyncindexperiod=5000 \
+    -resyncmempoolperiod=5000 \
+    -blockheight=11069956 \
+    -workers=1 \
+    -logtostderr
