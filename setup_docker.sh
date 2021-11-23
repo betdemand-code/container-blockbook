73d1bef3fc06e4c3c54c7ffa354f4f0a4549eb95
Yusaku Senga <yusaku@swingby.network>
2021-11-24 01:32:01 +0900

    fix: update ntp installer

--- setup_docker.sh
+++ setup_docker.sh
@@ -10,7 +10,7 @@ if ! type "docker-compose" >/dev/null; then
     sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
-if ! type "ntp" > /dev/null; then
+if ! sh -c "service ntp status" > /dev/null; then
     apt-get update
     apt-get install -y ntp
     service ntp start
