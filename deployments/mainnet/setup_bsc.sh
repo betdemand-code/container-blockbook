0afdff1b197b550cf6ec12bc30608c40b28312b6
syrohei <syrohei@gmail.com>
2022-01-24 12:44:21 +0800

    faet: update deploy logic for bsc node

--- deployments/mainnet/setup_bsc.sh
+++ deployments/mainnet/setup_bsc.sh
@@ -12,8 +12,11 @@ fi
 cd "$dir/mainnet/bsc"
 wget --no-check-certificate $(curl -s https://api.github.com/repos/binance-chain/bsc/releases/latest | grep browser_ | grep mainnet | cut -d\" -f4)
 unzip -o mainnet.zip
+mv mainnet/genesis.json ./genesis.json
+mv mainnet/config.toml ./config.toml
 rm -f mainnet.zip
 rm -f mainnet.zip.*
+rm -rf __MACOSX
 
 sed -i '/HTTPHost/d' "$dir/mainnet/bsc/config.toml"
 
